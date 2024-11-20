924e5c939013aa0c8a87a2095e94bba4fa3fb64d
Joe Zhe Piao <joellamaniapark@gmail.com>
2024-11-20 12:58:42 -0500

    checked in step3

--- src/frontend/src/app/pages/counter/ui.component.ts
+++ src/frontend/src/app/pages/counter/ui.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter-ui',
@@ -6,11 +6,28 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   imports: [],
   template: `
     <div data-testid="counter-feature-ui">
-      <button class="btn btn-primary">-</button>
-      <span data-testid="current">0</span>
-      <button class="btn btn-primary">+</button>
+      <button
+        class="btn btn-primary"
+        (click)="substract()"
+        [disabled]="this.counter() < 1"
+      >
+        -
+      </button>
+      <span data-testid="current" class="p-6">{{ this.counter() }}</span>
+      <button class="btn btn-primary" (click)="add()">+</button>
     </div>
   `,
   styles: ``,
 })
-export class UiComponent {}
+export class UiComponent {
+  counter = signal(0);
+  step = 1;
+  substract() {
+    if (this.counter() > 0) {
+      this.counter.update(() => this.counter() - this.step);
+    }
+  }
+  add() {
+    this.counter.update(() => this.counter() + this.step);
+  }
+}
