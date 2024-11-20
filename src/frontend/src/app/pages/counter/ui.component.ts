ec680e7abc5b8cf8e0cf6546e1219f342906acf3
Joe Zhe Piao <joellamaniapark@gmail.com>
2024-11-20 13:10:55 -0500

    step4

--- src/frontend/src/app/pages/counter/ui.component.ts
+++ src/frontend/src/app/pages/counter/ui.component.ts
@@ -1,4 +1,9 @@
-import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  signal,
+  computed,
+} from '@angular/core';
 
 @Component({
   selector: 'app-counter-ui',
@@ -16,12 +21,31 @@ import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
       <span data-testid="current" class="p-6">{{ this.counter() }}</span>
       <button class="btn btn-primary" (click)="add()">+</button>
     </div>
+    <div>
+      <p data-testid="fizzBuzz" class="p-6">{{ this.fizzBuzz() }}</p>
+    </div>
   `,
   styles: ``,
 })
 export class UiComponent {
   counter = signal(0);
   step = 1;
+  fizzBuzz = computed(() => {
+    let currCounter = this.counter();
+    if (currCounter > 0) {
+      if (currCounter % 3 === 0 && currCounter % 5 === 0) {
+        return 'FizzBuzz';
+      } else if (currCounter % 3 === 0) {
+        return 'Fizz';
+      } else if (currCounter % 5 === 0) {
+        return 'Buzz';
+      } else {
+        return '';
+      }
+    } else {
+      return '';
+    }
+  });
   substract() {
     if (this.counter() > 0) {
       this.counter.update(() => this.counter() - this.step);
